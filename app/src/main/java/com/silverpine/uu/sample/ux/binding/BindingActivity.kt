dda4d5c41bac0823f69b60136a74094d9f1c0238
Ryan DeVore <ryan@silverpine.com>
2023-02-02 11:24:30 -0800

    wire up dynamic binding stuff

--- app/src/main/java/com/silverpine/uu/sample/ux/binding/BindingActivity.kt
+++ app/src/main/java/com/silverpine/uu/sample/ux/binding/BindingActivity.kt
@@ -3,7 +3,11 @@ package com.silverpine.uu.sample.ux.binding
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.silverpine.uu.core.UUResources
+import com.silverpine.uu.core.uuGetOrNull
+import com.silverpine.uu.sample.ux.R
 import com.silverpine.uu.sample.ux.databinding.ActivityBindingBinding
+import com.silverpine.uu.ux.UUBorderedImageView
 
 class BindingActivity : AppCompatActivity()
 {
@@ -19,5 +23,31 @@ class BindingActivity : AppCompatActivity()
         binding.lifecycleOwner = this
         setContentView(binding.root)
         viewModel.update()
+
+        val borderViews: ArrayList<UUBorderedImageView> = arrayListOf(
+            binding.bordered0,
+            binding.bordered1,
+            binding.bordered2,
+            binding.bordered3,
+            binding.bordered4,
+            binding.bordered5,
+        )
+
+        viewModel.onTapBorderView =
+        { index ->
+            borderViews.uuGetOrNull(index)?.apply()
+            {
+                if (borderColor == UUResources.getColor(R.color.red))
+                {
+                    borderColor = UUResources.getColor(R.color.yellow)
+                    borderWidth = 40.0f
+                }
+                else
+                {
+                    borderColor = UUResources.getColor(R.color.red)
+                    borderWidth = 20.0f
+                }
+            }
+        }
     }
 }
