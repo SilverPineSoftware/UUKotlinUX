56f365345ab859d37a556e85b6d1d1becbb47bc1
Ryan DeVore <ryan@silverpine.com>
2023-08-08 12:16:56 -0700

    Add UUCircleView

---
+++ library/src/main/java/com/silverpine/uu/ux/UUCircleView.kt
@@ -0,0 +1,138 @@
+package com.silverpine.uu.ux
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.View
+import androidx.annotation.ColorRes
+import androidx.annotation.DimenRes
+import androidx.databinding.BindingAdapter
+import com.silverpine.uu.core.UUResources
+
+open class UUCircleView(context: Context, attrs: AttributeSet?, defStyle: Int): View(context, attrs, defStyle)
+{
+    private var _fillColor: Int = resources.getColor(android.R.color.transparent, null)
+    private val clippingPath = Path()
+    private var clipX: Float = 0.0f
+    private var clipY: Float = 0.0f
+    private var clipRadius: Float = 0.0f
+    private val borderPaint: Paint = Paint()
+
+    constructor(context: Context): this(context, null, 0)
+    constructor(context: Context, attrs: AttributeSet?): this(context, attrs, 0)
+
+    init
+    {
+        borderPaint.isDither = true
+        borderPaint.isAntiAlias = true
+        borderPaint.color = Color.TRANSPARENT
+        borderPaint.strokeWidth = 0.0f
+        borderPaint.strokeCap = Paint.Cap.ROUND
+        borderPaint.style = Paint.Style.STROKE
+
+        attrs?.let()
+        {
+            val a = context.obtainStyledAttributes(attrs, R.styleable.UUCircleView)
+
+            val borderWidth = a.getDimensionPixelSize(R.styleable.UUCircleView_uuBorderWidth, -1)
+            if (borderWidth != -1)
+            {
+                borderPaint.strokeWidth = borderWidth.toFloat()
+            }
+
+            val borderColor = a.getColor(R.styleable.UUCircleView_uuBorderColor, -1)
+            if (borderColor != -1)
+            {
+                borderPaint.color = borderColor
+            }
+
+            val fillColor = a.getColor(R.styleable.UUCircleView_uuFillColor, -1)
+            if (fillColor != -1)
+            {
+                this.fillColor = fillColor
+            }
+
+            a.recycle()
+        }
+    }
+
+    var borderColor: Int
+        get() = borderPaint.color
+        set(value) = internalSetBorderColor(value)
+
+    var borderWidth: Float
+        get() = borderPaint.strokeWidth
+        set(value) = internalSetBorderWidth(value)
+
+    var fillColor: Int
+        get() = _fillColor
+        set(value) = internalSetFillColor(value)
+
+    private fun internalSetBorderColor(color: Int)
+    {
+        borderPaint.color = color
+        invalidate()
+    }
+
+    private fun internalSetBorderWidth(width: Float)
+    {
+        borderPaint.strokeWidth = width
+        invalidate()
+    }
+
+    private fun internalSetFillColor(color: Int)
+    {
+        _fillColor = color
+        invalidate()
+    }
+
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int)
+    {
+        super.onLayout(changed, left, top, right, bottom)
+
+        if (changed)
+        {
+            clippingPath.reset()
+            val w = right - left
+            val h = bottom - top
+            clipX = w / 2.0f
+            clipY = h / 2.0f
+            clipRadius = w.coerceAtMost(h) / 2.0f
+            clippingPath.addCircle(clipX, clipY, clipRadius, Path.Direction.CW)
+        }
+    }
+
+    open fun preDraw(canvas: Canvas)
+    {
+        canvas.clipPath(clippingPath)
+        canvas.drawColor(fillColor)
+    }
+
+    override fun onDraw(canvas: Canvas)
+    {
+        preDraw(canvas)
+        super.onDraw(canvas)
+        canvas.drawCircle(clipX, clipY, clipRadius, borderPaint)
+    }
+}
+
+@BindingAdapter("uuBoundBorderColor")
+fun uuBindBorderColor(view: UUCircleView, @ColorRes resourceId: Int)
+{
+    view.borderColor = UUResources.getColor(resourceId)
+}
+
+@BindingAdapter("uuBoundBorderWidth")
+fun uuBindBorderWidth(view: UUCircleView, @DimenRes resourceId: Int)
+{
+    view.borderWidth = UUResources.getDimension(resourceId)
+}
+
+@BindingAdapter("uuBoundFillColor")
+fun uuBindFillColor(view: UUCircleView, @ColorRes resourceId: Int)
+{
+    view.fillColor = UUResources.getColor(resourceId)
+}
