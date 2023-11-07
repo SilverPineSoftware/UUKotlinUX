f1adca1176a523659d5408315e652dededfd8834
Ryan DeVore <ryan@silverpine.com>
2023-11-06 21:18:21 -0800

    remove parcelable

--- library/src/main/java/com/silverpine/uu/ux/UUButton.kt
+++ library/src/main/java/com/silverpine/uu/ux/UUButton.kt
@@ -1,12 +1,18 @@
 package com.silverpine.uu.ux
 
-import android.os.Parcelable
 import androidx.annotation.StringRes
 import com.silverpine.uu.core.UUResources
-import kotlinx.parcelize.Parcelize
 
-@Parcelize
-open class UUButton(var title: String, var action: ()->Unit): Parcelable
+open class UUButton()
 {
-    constructor(@StringRes resourceId: Int, action: ()->Unit): this(UUResources.getString(resourceId), action)
+    var title: String = ""
+    var action: ()->Unit = {   }
+
+    constructor(title: String, action: ()->Unit = { }): this()
+    {
+        this.title = title
+        this.action = action
+    }
+
+    constructor(@StringRes titleResourceId: Int, action: ()->Unit = { }): this(UUResources.getString(titleResourceId), action)
 }
--- library/src/main/java/com/silverpine/uu/ux/UUEditText.kt
+++ library/src/main/java/com/silverpine/uu/ux/UUEditText.kt
@@ -1,8 +1,8 @@
 package com.silverpine.uu.ux
 
-import android.os.Parcelable
 import android.text.InputType
-import kotlinx.parcelize.Parcelize
 
-@Parcelize
-class UUEditText(var hint: String = "", var text: String = "", var inputType: Int = InputType.TYPE_CLASS_TEXT): Parcelable
+open class UUEditText(
+    var hint: String = "",
+    var text: String = "",
+    var inputType: Int = InputType.TYPE_CLASS_TEXT)
