cc8d3d746aaaa3690baebce061f4e7fb1780c024
Ryan DeVore <ryan@silverpine.com>
2023-11-12 21:49:03 -0800

    add icon field to UUButton

--- library/src/main/java/com/silverpine/uu/ux/UUButton.kt
+++ library/src/main/java/com/silverpine/uu/ux/UUButton.kt
@@ -1,11 +1,13 @@
 package com.silverpine.uu.ux
 
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import com.silverpine.uu.core.UUResources
 
 open class UUButton()
 {
     var title: String = ""
+    @DrawableRes var icon: Int? = null
     var action: ()->Unit = {   }
 
     constructor(title: String, action: ()->Unit = { }): this()
@@ -15,4 +17,14 @@ open class UUButton()
     }
 
     constructor(@StringRes titleResourceId: Int, action: ()->Unit = { }): this(UUResources.getString(titleResourceId), action)
+
+    companion object
+    {
+        fun iconButton(@DrawableRes iconResourceId: Int, action: () -> Unit): UUButton
+        {
+            val button = UUButton("", action)
+            button.icon = iconResourceId
+            return button
+        }
+    }
 }
