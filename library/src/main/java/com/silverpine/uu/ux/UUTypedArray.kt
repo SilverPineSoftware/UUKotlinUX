fa02da42ea4afbecc3941193654d887f260deaac
Ryan DeVore <ryan@silverpine.com>
2023-12-10 05:38:06 -0800

    Add more safe gettors

--- library/src/main/java/com/silverpine/uu/ux/UUTypedArray.kt
+++ library/src/main/java/com/silverpine/uu/ux/UUTypedArray.kt
@@ -2,7 +2,6 @@ package com.silverpine.uu.ux
 
 import android.content.res.TypedArray
 import androidx.annotation.StyleableRes
-import java.lang.Exception
 
 fun TypedArray.uuGetInteger(@StyleableRes resId: Int, defaultValue: Int): Int
 {
@@ -57,6 +56,54 @@ fun TypedArray.uuGetColor(@StyleableRes resId: Int, defaultValue: Int): Int
     return try
     {
         getColor(resId, defaultValue)
+    }
+    catch (ex: Exception)
+    {
+        defaultValue
+    }
+}
+
+fun TypedArray.uuGetString(@StyleableRes resId: Int): String?
+{
+    return try
+    {
+        getString(resId)
+    }
+    catch (ex: Exception)
+    {
+        null
+    }
+}
+
+fun TypedArray.uuGetNonResourceString(@StyleableRes resId: Int): String?
+{
+    return try
+    {
+        getNonResourceString(resId)
+    }
+    catch (ex: Exception)
+    {
+        null
+    }
+}
+
+fun TypedArray.uuSafeGetString(@StyleableRes resId: Int, defaultValue: String): String
+{
+    return try
+    {
+        getString(resId) ?: defaultValue
+    }
+    catch (ex: Exception)
+    {
+        defaultValue
+    }
+}
+
+fun TypedArray.uuSafeGetNonResourceString(@StyleableRes resId: Int, defaultValue: String): String
+{
+    return try
+    {
+        getNonResourceString(resId) ?: defaultValue
     }
     catch (ex: Exception)
     {
