1f8d1412ddcf5c915895c6a59d7a0353917d5cbc
Ryan DeVore <ryan@silverpine.com>
2024-02-26 19:00:38 -0800

    wire up alert and activity methods

--- library/src/main/java/com/silverpine/uu/ux/viewmodel/UUViewModel.kt
+++ library/src/main/java/com/silverpine/uu/ux/viewmodel/UUViewModel.kt
@@ -2,18 +2,24 @@ package com.silverpine.uu.ux.viewmodel
 
 import android.app.Activity
 import android.os.Build
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModel
+import com.silverpine.uu.ux.UUAlertDialog
 import com.silverpine.uu.ux.UUToast
 import com.silverpine.uu.ux.uuHideKeyboard
 import com.silverpine.uu.ux.uuOpenSystemSettings
 import com.silverpine.uu.ux.uuOpenUrl
+import com.silverpine.uu.ux.uuShowAlertDialog
 import com.silverpine.uu.ux.uuShowToast
+import com.silverpine.uu.ux.uuStartActivity
 import com.silverpine.uu.ux.uuVibrate
 
 open class UUViewModel: ViewModel()
 {
     var uuHideKeyboard: ()->Unit = { }
-    //var uuShowAlertDialog: (UUAlertDialog)->Unit = { }
+    var uuShowAlertDialog: (UUAlertDialog)->Unit = { }
+    var uuStartActivity: (Class<out AppCompatActivity>, Bundle?)->Unit = { _, _ -> }
     var uuOpenUrl: (String)->Unit = { }
     var uuVibrate: (Int)->Unit = { }
     var uuToast: (UUToast)->Unit = { }
@@ -31,6 +37,7 @@ fun Activity.uuSetupViewModel(viewModel: UUViewModel)
     }
 
     viewModel.uuToast = this::uuShowToast
-    //viewModel.showAlertDialog = this::zzShowAlertDialog
     viewModel.uuOpenSystemSettings = this::uuOpenSystemSettings
+    viewModel.uuShowAlertDialog = this::uuShowAlertDialog
+    viewModel.uuStartActivity = this::uuStartActivity
 }
